8a16511c405dae9daf81dd98f8851a01b7a7a046
amirsmart <amirsmart1393@gmail.com>
2022-10-08 10:01:17 +0330

    optimize codes

--- main.py
+++ main.py
@@ -5,89 +5,71 @@ from PIL import Image
 import time
 from os.path import isfile , join
 from os import listdir
+import os
 import string
 import skimage
 
-
-words = [
-'O','N','O','I','T','A','L','F','E','D','R','P',
-'U','R','O','T','A','C','I','L','P','P','A','D',
-'T','L','C','O','N','V','E','Y','A','N','C','E',
-'F','S','C','H','E','M','A','H','E','S','D','A',
-'C','T','R','T','D','U','L','L','Y','S','I','R',
-'A','I','E','L','S','T','P','O','B','E','S','E',
-'B','T','U','L','I','A','P','Y','N','E','L','F',
-'B','E','M','I','S','T','E','R','T','I','L','H',
-'Y','L','S','U','O','I','C','I','P','S','U','S',
-'P','B','J','D','V','O','I','C','P','H','F','L',
-'T','A','N','K','E','N','R','H','A','N','S','A',
-'O','T','N','E','L','E','P','P','A','R','N','B',
-'L','L','D','E','M','R','A','H','C','L','U','I',
-'F','L','A','M','E','N','C','O','U','R','E','D',
-'C','U','H','A','N','D','I','W','O','R','K','F'
-]
+# making cache directory 
+if not os.path.exists('cache'):
+    print("[*]  Creating cache directory ...")
+    os.makedirs('cache')
 
 
+# size of puzzle box from top-left window
 bounding_box = {'top': 200, 'left': 270, 'width': 430, 'height': 550}
-ROW = 15
-COLUMN = 12
+ROW = 15 # puzzle row length
+COLUMN = 12 # puzzle column length
 
+# using mss for capturing screen
 sct = mss()
 
+# compare target image with alphabet assets and return score
 def compare(targetImage,mypath = 'assets'):
-    cachFiles = [f[:-4] for f in listdir(mypath) if isfile(join(mypath, f))]
-
-    cachFiles.sort()
-    res = ''
-    mx = 0
-    score = -1
-    for p in cachFiles:
+    assetFiles = [f[:-4] for f in listdir(mypath) if isfile(join(mypath, f))] # load asset files
+    assetFiles.sort() # sort asset files alphabetically
+    targetLetter = '' # init target letter
+    maxScore = 0 # maxScore of comapre results
+    score = -1 # final score
+    for p in assetFiles:
         p = str(p) + '.png'
-        image_1 = cv2.imread(mypath + '/' + p, 0)
-        image_2 = cv2.imread('cache' + '/' + targetImage, 0)
+        image_1 = cv2.imread(mypath + '/' + p, 0) # asset image
+        image_2 = cv2.imread('cache' + '/' + targetImage, 0) # targetImage
         
-        (H, W) = image_1.shape
+        (H, W) = image_1.shape # get assetImage size
         # to resize and set the new width and height 
         image_2 = cv2.resize(image_2, (W, H))
-        (score, diff) = skimage.metrics.structural_similarity(image_1, image_2, full=True)
-        ccc= score
-        #print(p , mx , ccc , p2,'\n--------')
-        if ccc >= mx:
-            res = p
-            mx = ccc
+        (score, diff) = skimage.metrics.structural_similarity(image_1, image_2, full=True) # get similarity score of asset and target image
+        # find maximum score and set targetLetter
+        if score >= maxScore:
+            targetLetter = p
+            maxScore = score
 
-    if score >= -1:
-        return res[0]
-    else:
-        pass
-        print(score , res[0])
+    if maxScore >= -1:
+        return targetLetter[0]
+
 
 
 while True:
-
+    # take screenshot of puzzle box 
     sct_img = sct.grab(bounding_box)
+    # convert screenshot to numpy array
     img = np.array(sct_img)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    h, w , _ = img.shape
+    # vertical distance between each words in puzzle box
     vertival_devider = 35
-    vertival2_devider = 35
-    
+    # hor distance between each words in puzzle box
     hor_devider = 35
-    hor2_devider = 35
     
-    data = []
+    # for loop on each word of puzzle box
     for i in range(ROW):
         for j in range(COLUMN):
-            word = img[i*vertival2_devider:(i*vertival2_devider)+vertival_devider , (j)*hor2_devider:((j)*hor2_devider)+hor_devider]
+            # cut the target word
+            word = img[i*vertival_devider:(i+1)*vertival_devider , (j)*hor_devider:(j+1)*hor_devider]
+            # write target word as cache file
             cv2.imwrite('cache/%d%d.png'%(i,j),word )
+            # compare it with assets
             res = compare('%d%d.png'%(i,j))
-            pointer = (i * 12) + j
-            if res == None:
-                res = '-'
-            if res != words[pointer]:
-                print(res + '-' + words[pointer] , end=' ')
-            else:
-                print(res  , end=' ')
+            # print the result
+            print(res  , end=' ')
                 
         print()
     break
