975a04893fd6a6c57ab1ad5d5a7932c4d3be8262
amirsmart <amirsmart1393@gmail.com>
2022-10-11 08:26:05 +0330

    all done

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import numpy as np
 import cv2
 from mss import mss
-from PIL import Image
-import time
 from os.path import isfile , join
 from os import listdir
 import os
-import string
 import skimage
+from colored import fg, bg, attr
 
 # making cache directory 
 if not os.path.exists('cache'):
@@ -47,14 +45,84 @@ def compare(targetImage,mypath = 'assets'):
     if maxScore >= -1:
         return targetLetter[0]
 
-def solver(pBox , word):
-    pass
+def sub_solver(x,y,word,pBox,move,pointer=1,result=[]):
+    # table of avaiable moves
+    moves = {
+        'r':[0,1],
+        'l':[0,-1],
+        'u':[1,0],
+        'd':[-1,0],
+        'ru':[1,1],
+        'rd':[-1,1],
+        'lu':[1,-1],
+        'ld':[-1,-1]
+    }
+    # this flag is for checking the final result
+    flag = False
+    # if the pointer is bigger than target word return flag
+    if pointer >= len(word):
+        return flag
+    
+    # get target direction and convert it to 2d
+    i , j = moves[move]
+    # get previous alphabet location 
+    xy1 = (x * COLUMN) + y
+    # get current alphabet 2d location
+    x2 = x + i
+    y2 = y + j
+    # convert 2d location to 1d location
+    xy2 = (x2 * COLUMN) + y2
+    # check if 1d location is in puzzle box
+    cond1 = xy2 > 0 and xy2 < len(pBox)
+    # check if y2 is in COLUMN range
+    cond2 = y2 >=0 and y2 < COLUMN
+    # check if x2 is in ROW range
+    cond3 = x2 >= 0 and x2 < ROW
+    # check if current location is not equal with previous location
+    cond4 = xy1 != xy2 
+    # check if all conditions is True
+    if  cond1 and cond2 and cond3 and cond4:
+        # check if current alphabet is equal with previous alphabet
+        if pBox[xy2] == word[pointer]:
+            # if it is last one return the answer
+            if pointer + 1 == len(word):
+                return [*result,xy2]
+            # check if we can get the answer
+            flag = flag or sub_solver(x2,y2,word,pBox,move,pointer+1,[*result,xy2])
+            
 
-# print the puzzle box in right format
-def print_puzzle_box(pBox):
+    return flag
+        
+def solver(pBox , word):
+    # check all cells
     for i in range(ROW):
         for j in range(COLUMN):
-            print(pBox[(i * COLUMN) + j] ,end=' ')
+            # if starting alphabet of word is equal with cell alphabet
+            if word[0] == pBox[(i * COLUMN) + j]:
+                # all avaiables directions
+                directions = ['r','l','u','d','ru','rd',
+                    'lu','ld']
+                # check if we can get the answer from all dircetions
+                for direction in directions:
+                    # check the answer
+                    result = sub_solver(i,j , word,pBox,direction)
+                    if result :
+                        result =[(i * COLUMN) + j , *result]
+                        # print the answer in another color
+                        print_puzzle_box(pBox,result)
+                        pass
+    
+
+# print the puzzle box in right format
+def print_puzzle_box(pBox , select=[]):
+    for i in range(ROW):
+        for j in range(COLUMN):
+            xy = (i * COLUMN) + j
+            if xy in select:
+                print(f"{fg(1)}{attr(5)}{pBox[xy]}{attr('reset')}" ,end=' ')
+            else:
+                print(pBox[xy] ,end=' ')
+                
         print()
 
 # take screenshot of puzzle box 
@@ -89,6 +157,8 @@ word_number = 1
 # get words from user
 while True:
     word = input("input word number%d or press q for quit: "%(word_number))
+    # convert input to uppercase
+    word = word.upper()
     # if target word == q exit from app
     if word == 'q':
         exit(0)
