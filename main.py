bd49f8e9ba848cb53b0476d0990bc5724deee06c
Nima <56191102+nima-kam@users.noreply.github.com>
2022-10-26 18:50:28 +0330

    mouse word selection
    using the pygui lib, the word selection is automated and now, is automatically done after finding the word

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import select
 import numpy as np
 import cv2
 from mss import mss
@@ -6,6 +7,7 @@ from os import listdir
 import os
 import skimage
 from colored import fg, bg, attr
+import pyautogui as pag
 
 # making cache directory 
 if not os.path.exists('cache'):
@@ -18,8 +20,16 @@ bounding_box = {'top': 200, 'left': 270, 'width': 430, 'height': 550}
 ROW = 15 # puzzle row length
 COLUMN = 12 # puzzle column length
 
+# vertical distance between each alphabets in puzzle box
+vertival_devider = 35
+# hor distance between each alphabets in puzzle box
+hor_devider = 35
+
+
 # using mss for capturing screen
 sct = mss()
+
+
 
 # compare target image with alphabet assets and return score
 def compare(targetImage,mypath = 'assets'):
@@ -93,6 +103,61 @@ def sub_solver(x,y,word,pBox,move,pointer=1,result=[]):
 
     return flag
         
+def drag_mouse(cord1,cord2):
+    """
+    drag mouse from cordinate 1(cord1) to cordinate 2(cord2) on the screen
+    """
+    pag.moveTo(x=cord1[0],y=cord1[1],duration=0.3)
+
+    pag.mouseDown()
+    pag.mouseDown(x=cord2[0],y=cord2[1],duration=1)
+    pag.dragTo(x=cord2[0],y=cord2[1],duration=0.5)
+    pag.mouseUp()
+
+def get_index2d(index1d:int,height:int,width:int) :
+    """
+    return the equivalant 2d index of the i-th element 
+
+    width: number of columns
+    height: number of rows
+
+    """
+    if index1d >= width * height:
+        raise IndexError()
+    
+    j = index1d // width  
+    i = index1d % width
+    return i, j
+
+def get_cordinates(i,j):
+    start_x=bounding_box['left']+6
+    start_y=bounding_box['top']+6
+    x=start_x + i * hor_devider
+    y=start_y + j * vertival_devider
+
+    return x, y
+
+
+def select_word(result:list[int]) :
+    """
+    getting the result alphabet indexes selects the word,
+    by dragging the cursor and selecting the word
+
+    """
+
+    # get the index of start and ending alphabet in the grid
+    ind1 = get_index2d(result[0],ROW,COLUMN)
+    ind2 = get_index2d(result[-1],ROW,COLUMN)
+
+    # get the cordinate of corresponding indexes
+    cord1 = get_cordinates(*ind1)   # starting point
+    cord2 = get_cordinates(*ind2)   # ending point
+
+    # move mouse and select the word 
+    drag_mouse(cord1, cord2)  
+
+
+
 def solver(pBox , word):
     # check all cells
     for i in range(ROW):
@@ -110,8 +175,11 @@ def solver(pBox , word):
                         result =[(i * COLUMN) + j , *result]
                         # print the answer in another color
                         print_puzzle_box(pBox,result)
+                        # select the founded word by mouse
+                        select_word(result)
                         pass
     
+
 
 # print the puzzle box in right format
 def print_puzzle_box(pBox , select=[]):
@@ -125,43 +193,49 @@ def print_puzzle_box(pBox , select=[]):
                 
         print()
 
-# take screenshot of puzzle box 
-sct_img = sct.grab(bounding_box)
-# convert screenshot to numpy array
-img = np.array(sct_img)
-# vertical distance between each alphabets in puzzle box
-vertival_devider = 35
-# hor distance between each alphabets in puzzle box
-hor_devider = 35
+def detect():
+    """
+    start detecting alphabets and words in the game
+    """        
 
-puzzle_box = []
+    # take screenshot of puzzle box 
+    sct_img = sct.grab(bounding_box)
+    # convert screenshot to numpy array
+    img = np.array(sct_img)
+    
 
-print("[*]  Scanning puzzle box ...")
+    puzzle_box = []
 
-# for loop on each alphabet of puzzle box
-for i in range(ROW):
-    for j in range(COLUMN):
-        # cut the target alphabet
-        alphabet = img[i*vertival_devider:(i+1)*vertival_devider , (j)*hor_devider:(j+1)*hor_devider]
-        # write target alphabet as cache file
-        cv2.imwrite('cache/%d%d.png'%(i,j),alphabet )
-        # compare it with assets
-        res = compare('%d%d.png'%(i,j))
-        # add the alphabet to our box
-        puzzle_box.append(res)
-# print the puzzle box after scan        
-print_puzzle_box(puzzle_box)   
+    print("[*]  Scanning puzzle box ...")
 
-# count number of input words
-word_number = 1
-# get words from user
-while True:
-    word = input("input word number%d or press q for quit: "%(word_number))
-    # convert input to uppercase
-    word = word.upper()
-    # if target word == q exit from app
-    if word == 'q':
-        exit(0)
-    # solve the puzzle
-    result = solver(puzzle_box , word)
-    word_number += 1
+    # for loop on each alphabet of puzzle box
+    for i in range(ROW):
+        for j in range(COLUMN):
+            # cut the target alphabet
+            alphabet = img[i*vertival_devider:(i+1)*vertival_devider , (j)*hor_devider:(j+1)*hor_devider]
+            # write target alphabet as cache file
+            cv2.imwrite('cache/%d%d.png'%(i,j),alphabet )
+            # compare it with assets
+            res = compare('%d%d.png'%(i,j))
+            # add the alphabet to our box
+            puzzle_box.append(res)
+    # print the puzzle box after scan        
+    print_puzzle_box(puzzle_box)   
+
+    # count number of input words
+    word_number = 1
+    # get words from user
+    while True:
+        word = input("input word number%d or press q for quit: "%(word_number))
+        # convert input to uppercase
+        word = word.upper()
+        # if target word == q exit from app
+        if word == 'q':
+            exit(0)
+        # solve the puzzle
+        result = solver(puzzle_box , word)
+        word_number += 1
+
+
+if __name__ == '__main__':
+    detect()
